f45cd03660c8ce488d813d42211f844b289e5a54
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-12-17 09:19:44 +0530

    build the menu for mobile

--- src/components/Header/MenuItems/MenuItems.jsx
+++ src/components/Header/MenuItems/MenuItems.jsx
@@ -1,8 +1,27 @@
 
 
 const MenuItems = () => {
+  const moreOnAbleton=[
+    {
+      title:"Loop",
+      paragraph:"Watch Talks, performances, and features from Ableton's summit for music makers."
+    },
+    {
+      title:"Learning Music",
+      paragraph:"Learn the fundamentals of music making right in your browser."
+    },
+     {
+      title: "Learning Synths",
+      paragraph:"Get started with synthesis using a web-based synth and accompanying lessons. "
+    },
+     {
+      title:"Making Music",
+      paragraph:"SOme tips from 74 Creative Strategies for Electronic Producers."
+    }
+   
+  ]
   return (
-    <div className=' md:hidden w-[100%] h-[700px] bg-[#0000ff] py-3'>
+    <div className=' md:hidden w-[100%] bg-[#0000ff] py-3 text-white'>
       <div className="mx-6 flex gap-3 flex-col">
       <div>
         <p className=" font-semibold cursor-pointer">Live</p>
@@ -58,7 +77,21 @@ const MenuItems = () => {
         <div>
           <p className="text-[.8rem] cursor-pointer">Apprenticeships</p>
         </div>
-        <div></div>
+        <div>
+          <h1 className="font-semibold">More from Ableton:</h1>
+        </div>
+        
+  <div  className="flex flex-row gap-5 overflow-x-scroll text-[.75rem]">
+   {moreOnAbleton.map((item, index) => (
+    <div key={index} className=" cursor-pointer p-1 flex gap-1 flex-col max-w-[300px]">
+      <h2 className="font-semibold">{item.title}</h2>
+      <p className="">{item.paragraph}</p>
+    </div>
+    ))}
+  </div>
+
+
+
       </div>
       </div>
 
